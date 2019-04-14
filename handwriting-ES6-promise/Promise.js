6cfa1bf0981d679097821b90fa758075dbd88f0c
YuTengjing <2713151713@qq.com>
2019-04-14 20:47:58 +0800

    Update: MyPromise basicly complete

--- handwriting-ES6-promise/Promise.js
+++ handwriting-ES6-promise/Promise.js
@@ -16,24 +16,26 @@ class MyPromise {
             setTimeout(() => {
                 if (this.status === Status.PENDING) {
                     this.status = Status.FULFILLED;
-                    this.onFulfilledCallbacks.forEach(cb => cb);
+                    this.value = value;
+                    this.onFulfilledCallbacks.forEach(cb => cb());
                 }
             });
         }
 
-        const reject = (value) => {
+        const reject = (reason) => {
             setTimeout(() => {
                 if (this.status == Status.PENDING) {
-                    this.reason = value;
-                    this.onRejectedCallbacks(cb => cb());
+                    this.status = Status.REJECTED;
+                    this.reason = reason;
+                    this.onRejectedCallbacks.forEach(cb => cb());
                 }
             })
         }
 
         try {
             executor(resolve, reject);
-        } catch (err) {
-            reject(err);
+        } catch (reason) {
+            reject(reason);
         }
     }
 
@@ -41,7 +43,7 @@ class MyPromise {
         const self = this;
         onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
         onRejected = typeof onRejected === 'function' ? onRejected : err => {
-            throw err
+            throw err;
         };
 
         const promise2 = new MyPromise((resolve, reject) => {
@@ -50,8 +52,9 @@ class MyPromise {
                     setTimeout(() => {
                         try {
                             const x = onFulfilled(self.value);
-                        } catch (error) {
-                            reject();
+                            MyPromise.resolvePromise(promise2, x, resolve, reject);
+                        } catch (reason) {
+                            reject(reason);
                         }
                     })
                 })
@@ -59,33 +62,65 @@ class MyPromise {
                 self.onRejectedCallbacks.push(() => {
                     try {
                         const x = onRejected(self.reason);
-                    } catch (err) {
-                        reject();
+                        MyPromise.resolvePromise(promise2, x, resolve, reject);
+                    } catch (reason) {
+                        reject(reason);
+                    }
+                })
+            } else if (self.status === Status.FULFILLED) {
+                setTimeout(() => {
+                    try {
+                        const x = onFulfilled(self.value);
+                        MyPromise.resolve(promise2, x, resolve, reject);
+                    } catch (reason) {
+                        reject(reason);
+                    }
+                })
+            } else if (self.status === Status.REJECTED) {
+                setTimeout(() => {
+                    try {
+                        const x = onRejected(self.reason);
+                        MyPromise.resolvePromise(promise2, x, resolve, reject);
+                    } catch (reason) {
+                        reject(reason);
                     }
                 })
             }
         });
 
+        return promise2;
     }
 
-    static resolvePromise(promise2, x, reject, resolve) {
-        if (promise2 === returnValue) throw new TypeError('Chaining cycle detected for promise #<Promise></Promise>');
+    static resolvePromise(promise2, x, resolve, reject) {
+        if (promise2 === x) throw new TypeError('Chaining cycle detected for promise #<Promise></Promise>');
 
-        const isObjOrFunc = x !== null && (Object.prototype.toString.call(x) === '[object Object]' || Object.prototype.toString.call(x) === '[object Function]';
-        try {
+        const isObjOrFunc = x !== null && (Object.prototype.toString.call(x) === '[object Object]' || Object.prototype.toString.call(x) === '[object Function]');
+
+        if (isObjOrFunc) {
             const then = x.then;
+            if (typeof then === 'function') {
+                if (x.__called__) return;
 
-            if (isObjOrFunc) {
-                if (typeof then === 'function') {
+                try {
                     x.then(y => {
-                        resolvePromise(promise2, y, resolve, reject);
+                        x.__called__ = true;
+                        MyPromise.resolvePromise(promise2, y, resolve, reject);
                     }, reason => {
+                        x.__called__ = true;
                         reject(reason);
                     })
+                } catch (reason) {
+                    reject(reason);
                 }
+            } else {
+                resolve(x);
             }
-        } catch (err) {
-            
+        } else {
+            resolve(x);
         }
     }
+}
+
+if (require.main === module) {
+    
 }
