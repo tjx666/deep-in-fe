72e44f3aa0fb6c7af78aaea4421051987765d99e
YuTengjing <2713151713@qq.com>
2019-04-16 15:46:48 +0800

    Update: new Operator

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 2. EventEmitter √
 3. 类型检测
    + 手写 instanceOf √
-4. new 操作符
+4. new 操作符 √
 5. 原型继承
 6. 事件委托
 7. redux
---
+++ src/newOperator/_new.js
@@ -0,0 +1,8 @@
+const _new = (fn, ...args) => {
+    const target = Object.create(fn.prototype);
+    const result = fn.call(target, ...args);
+    const isObjectOrFunction = result !== null && (typeof result === 'object' || typeof result === 'function');
+    return isObjectOrFunction ? result : target;
+}
+
+module.exports = _new;
