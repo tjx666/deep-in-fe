430eade075613540129c74f8f3e68beba52966cc
YuTengjing <ytj2713151713@gmail.com>
2019-09-19 23:32:25 +0800

    :sparkles: Feature: add koa middleware mechanism

---
+++ src/koaMiddleware/kosMiddleware.js
@@ -0,0 +1,35 @@
+class Koa {
+    constructor() {
+        this.middlewares = [];
+    }
+
+    use(middleware) {
+        this.middlewares.push(middleware);
+    }
+
+    applyMiddlewares() {
+        let index = 0;
+        const next = async () => {
+            ++index < this.middlewares.length && (await this.middlewares[index](next));
+        };
+
+        !(async () => {
+            index < this.middlewares.length && (await this.middlewares[index](next));
+        })();
+    }
+}
+
+const koa = new Koa();
+koa.use(async function middleware1(next) {
+    console.log('middleware1 start');
+    await next();
+    console.log('middleware1 end');
+});
+
+koa.use(async function middleware2(next) {
+    console.log('middleware2 start');
+    await next();
+    console.log('middleware2 end');
+});
+
+koa.applyMiddlewares();
