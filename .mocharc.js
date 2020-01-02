53abbb62f7fcb76df427cfe911c94bdd49ef3c0e
YuTengjing <ytj2713151713@gmail.com>
2020-01-02 15:31:59 +0800

    :sparkles: feat: integrate ts

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,7 @@ module.exports = {
     require: ['@babel/register'],
     recursive: true,
     spec: ['./test/**/*.test.js'],
-    extension: 'js',
+    extension: ['js', 'ts'],
     exit: true,
     timeout: 3 * 1000,
     colors: true,
