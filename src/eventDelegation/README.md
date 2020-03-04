0754658f571afaf415de0c77be90dbf79c48f923
YuTengjing <ytj2713151713@gmail.com>
2020-03-04 18:23:34 +0800

    :recycle: refactor: optimize event delegate

--- src/eventDelegation/README.md
+++ src/eventDelegation/README.md
@@ -1,2 +1,7 @@
-参考了：[Javascript 事件委托详解](https://juejin.im/entry/58fd97a7b123db74d87eca14)
-![测试截图](https://github.com/tjx666/javascript-code-lab/blob/master/screenshots/event-delegation.png?raw=true)
+## :link: references
+
+- [Javascript 事件委托详解](https://juejin.im/entry/58fd97a7b123db74d87eca14)
+
+## 案例：九九乘法表
+
+![测试截图](https://github.com/tjx666/deep-in-fe/blob/master/images/event-delegation.png?raw=true)
--- src/eventDelegation/eventDelegate.js
+++ src/eventDelegation/eventDelegate.js
@@ -1,25 +1,36 @@
 /* eslint-disable no-unused-vars */
 
-const eventDelegate = (parentSelector, targetSelector, events, callback) => {
-    const delegateCallback = (event, ...args) => {
+/**
+ * 事件委托
+ *
+ * 好处：
+ * 1. 不需要在每个子元素上绑定回调，节省内存
+ * 2. 子元素频繁增删不需要重复绑定事件
+ *
+ * 局限性
+ * 1. 有些事件没有冒泡，例如 focus，blur
+ * 2. 任何子元素触发被代理的事件都会触发 wrappedListener，像 mouseover 这类会在代理元素内频繁触发的事件就不适合用事件委托，
+ * 单独绑定可以减少很多没必要的 wrappedListener 调用
+ *
+ * @param {string} event 事件名
+ * @param {string} parentSelector 代理元素的选择器
+ * @param {string} targetSelector 被代理元素的选择器
+ * @param {string} listener 事件回调
+ */
+function eventDelegate(event, parentSelector, targetSelector, listener) {
+    function wrappedListener(event, ...args) {
         let { target } = event;
         const { currentTarget } = event;
 
         while (target !== currentTarget) {
             if (target.matches(targetSelector)) {
-                callback.call(target, event, ...args);
+                listener.call(target, event, ...args);
             }
-
             target = target.parentNode;
         }
-    };
+    }
 
-    events
-        .trim()
-        .split(/\s+/)
-        .forEach(event => {
-            document.querySelectorAll(parentSelector).forEach(parentEle => {
-                parentEle.addEventListener(event, delegateCallback);
-            });
-        });
-};
+    document
+        .querySelectorAll(parentSelector)
+        .forEach(parent => parent.addEventListener(event, wrappedListener));
+}
